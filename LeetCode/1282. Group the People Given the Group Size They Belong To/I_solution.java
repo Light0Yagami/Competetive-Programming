9ed8a6d2c015cad609a46d7bd1297716a438c437
Ayush negi <79302271+Light0Yagami@users.noreply.github.com>
2022-11-19 14:02:45 +0530

    Create I_solution.java

---
+++ LeetCode/1282. Group the People Given the Group Size They Belong To/I_solution.java
@@ -0,0 +1,37 @@
+/*
+here are n people that are split into some unknown number of groups. Each person is labeled with a unique ID from 0 to n - 1.
+
+You are given an integer array groupSizes, where groupSizes[i] is the size of the group that person i is in. For example, 
+if groupSizes[1] = 3, then person 1 must be in a group of size 3.
+
+Return a list of groups such that each person i is in a group of size groupSizes[i].
+
+Each person should appear in exactly one group, and every person must be in a group. If there are multiple answers, 
+return any of them. It is guaranteed that there will be at least one valid solution for the given input.
+*/
+
+class Solution {
+    public static List<List<Integer>> groupThePeople(int[] groupSizes) {        
+        int n = groupSizes.length;
+        int i = 0,j=0;
+        List<List<Integer>> ans = new ArrayList<>();
+        
+        for(i = 1; i < n; i++){
+            List<Integer> subans =  new ArrayList<>();
+
+            for(j = 0; j< n; j++){
+                if(groupSizes[j] == i){
+                    if(subans.size() >= i){
+                        ans.add(subans);
+                        subans.clear();
+                    }
+                    subans.add(j);
+                }
+            }
+            if(subans.size() > 0){
+                ans.add(subans);
+            }
+        }
+        return ans;
+    }
+}
