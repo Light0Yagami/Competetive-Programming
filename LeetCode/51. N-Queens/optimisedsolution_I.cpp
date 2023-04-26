939fd5105d3a626dd461074a74c55327fb8c8ab4
Ayush negi <79302271+Light0Yagami@users.noreply.github.com>
2023-04-26 21:54:27 +0530

    Update optimisedsolution_I.cpp

--- LeetCode/51. N-Queens/optimisedsolution_I.cpp
+++ LeetCode/51. N-Queens/optimisedsolution_I.cpp
@@ -1,52 +1,37 @@
-vector<vector<string>> ans;
+  vector<vector<string>> ans;
     vector<vector<string>> solveNQueens(int n) {
         if(n==1) return{{"Q"}};
         
         string s(n,'.');
         vector<string> v(n,s);
         
-        
-        fun(0,n,v);
+        //col_wize fill ->optimize map
+        vector<int> RowChk(n,0),
+                    lorDiag((2*n)-1,0),
+                    uprDiag((2*n)-1,0);
+
+        fun(0,n,v,RowChk,lorDiag,uprDiag);
         return  ans;  
     }
-    void fun(int col,int n,vector<string> &v)
-    {
-        if(col==n) {
-            ans.push_back(v);
-            return;
-        }
-        for(int r=0;r<n;r++)
-            if(okTogo(r,col,n,v))
+    void fun(int col,int n,vector<string> &v,vector<int> &RowChk,vector<int>  &lorDiag,vector<int> &uprDiag){
+
+    if(col==n) {
+       ans.push_back(v); 
+       return;
+    }
+    for(int r=0;r<n;r++) //upr -> n-1-r+col
+       if(RowChk[r]==0 && lorDiag[r+col]==0 && uprDiag[n-1+col-r]==0)     
             {
                 v[r][col]='Q';
-                fun(col+1,n,v);
+                RowChk[r]=1;
+                lorDiag[r+col]=1;
+                uprDiag[n-1+col-r]=1; 
+                //next pos
+                fun(col+1,n,v,RowChk,lorDiag,uprDiag);
+                //backtrack
                 v[r][col]='.';
+                RowChk[r]=0;
+                lorDiag[r+col]=0;
+                uprDiag[n-1+col-r]=0; 
             }
-    }
-
-    bool okTogo(int r,int c,int n,vector<string> &v)
-    {
-        int row =r,col =c;
-        
-        //digonal check uppr left
-        while(r>=0 && c>=0)
-        {
-            if(v[r][c]=='Q') return false;
-            r--;c--;
-        }
-        // lower left diagonal
-        r=row;c=col;
-        while(r<n && c>=0)
-        {
-            if(v[r][c]=='Q') return false;
-            r++;c--;
-        }
-        
-         //check horizontal left side
-        while(col>=0)
-        {
-            if(v[row][col]=='Q') return false;
-            col--;
-        }
-        return true;
     }
